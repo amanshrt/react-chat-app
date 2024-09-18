ec6b137b4a1bed48fd183df84c271f1d113294a4
Subash Shrestha <119420796+arconsis-subash@users.noreply.github.com>
2024-09-18 09:33:03 +0200

    add: normal ribbon

--- src/presentation/pages/Home.tsx
+++ src/presentation/pages/Home.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React from 'react';
 import UploadDocument from "../components/UploadDocument";
-import ChatPage from "./ChatPage";
+import ChatPage from './ChatPage';
+import '../styles/index.css'; // Ensure this path matches your file structure
 
 const Home: React.FC = () => {
   return (
     <>
-      <UploadDocument/>
+      <div className="ribbon"><span>DEMO PURPOSE ONLY</span></div>
+      <UploadDocument />
       <ChatPage />
     </>
   );
 };
 
 export default Home;
+
